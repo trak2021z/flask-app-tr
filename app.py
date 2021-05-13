444ca206c47f21cc1d4b09ec51c17c5c06c0f129
Kamil Malita <kamil.malita@gmail.com>
2021-05-13 22:31:13 +0200

    Add first template and use python code in html template

--- app.py
+++ app.py
@@ -1,11 +1,22 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
+
+posts = [
+    {
+        'author': "Janusz",
+        'age': "15"
+    },
+    {
+        'author': "Maria",
+        'age': "23"
+    }
+]
 
 
 @app.route('/')
 def hello_world():
-    return 'Hello World!'
+    return render_template('home.html', posts=posts)
 
 
 if __name__ == '__main__':
